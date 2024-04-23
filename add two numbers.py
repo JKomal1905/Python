50a528ae414af427720b1a9c2113b519457a7776
JKomal1905 <167868124+JKomal1905@users.noreply.github.com>
2024-04-23 21:54:18 +0530

    Add files via upload

---
+++ add two numbers.py
@@ -0,0 +1,9 @@
+# a=float(input("enter a number"))
+# b=float(input("enter a number"))
+# sum=a+b
+# print(sum)
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+sum=a+b
+print(sum)
